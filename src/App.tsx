e6c7460536a6ddddc888d3c21da9b6abc4cd95a8
Mariia Bulchak <bulchakmariiast@gmail.com>
2025-05-01 09:03:39 +0300

    feat: add routing, add header, add pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,37 @@
+import {Outlet, useNavigate} from 'react-router';
 import './App.css';
+import {useEffect} from 'react';
 
 function App() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate('/dashboard');
+  }, []);
+
   return (
     <>
-      <h2>Hello World</h2>
-      <h3>dfsd</h3>
+      <div>
+        <div className='flex justify-between items-center h-[75px] bg-[#121622] text-white px-[100px] border-b border-[#6B88B4]'>
+          <h1 className='h-[44px] text-[24px] leading-[44px] '>
+            Hi Arthur, welcome! You have 12 open tasks .
+          </h1>
+
+          <div>
+            <input
+              className='border-none bg-[#1E2233] h-[36px] w-[360px] rounded-[22px] text-[12px] ps-[10px] mr-[6px]'
+              type='text'
+              placeholder='Search'
+            />
+
+            <button className='cursor-pointer w-[48px] h-[48px] rounded-[50px] bg-linear-to-b from-[#1E40AF] to[#1e40af47] bg-[#1e40af47]'>
+              AR
+            </button>
+          </div>
+        </div>
+
+        <Outlet />
+      </div>
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+import {BrowserRouter, Route, Routes} from 'react-router';
+import {Dashboard} from './pages/Dashboard/Dashboard.tsx';
+import {Accounts} from './pages/Accounts/Accounts.tsx';
+
+const Root = () => {
+  return (
+    <BrowserRouter basename='dashboard-dec'>
+      <Routes>
+        <Route path='/' element={<App />}>
+          <Route path='dashboard' element={<Dashboard />} />
+
+          <Route path='accounts' element={<Accounts />} />
+
+          <Route path='*' element={<p>Not found page</p>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Root />
+  </StrictMode>
+);
